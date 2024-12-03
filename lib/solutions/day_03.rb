cf9a347b6b9a20e2a792d001d952bc31d718cfb9
Ariejan de Vroom <ariejan@devroom.io>
2024-12-03 07:48:38 +0100

    Day 2 - Puzzle 2

--- lib/solutions/day_03.rb
+++ lib/solutions/day_03.rb
@@ -1,5 +1,7 @@
 class Day03
   REGEX_1 = /mul\((\d{1,3}),(\d{1,3})\)/
+  REGEX_2 = /(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))/
+
   def part_one(input)
     input.scan(REGEX_1).map do |match|
       match.map(&:to_i).inject(:*)
@@ -7,6 +9,22 @@ class Day03
   end
 
   def part_two(input)
-    0
+    matches = input.scan(REGEX_2)
+    result = 0
+    enabled = true
+
+    matches.each do |match|
+      instruction = match[0][0..2]
+      case instruction
+      when 'mul'
+        result += match[1].to_i * match[2].to_i if enabled
+      when 'do('
+        enabled = true
+      when 'don'
+        enabled = false
+      end
+    end
+
+    result
   end
 end
--- spec/solutions/day_03_spec.rb
+++ spec/solutions/day_03_spec.rb
@@ -12,7 +12,8 @@ RSpec.describe Day03 do
 
   describe '#part_two' do
     it 'calculates the correct solutions for part two' do
-      expect(subject.part_two(input)).to eq(0)
+      input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+      expect(subject.part_two(input)).to eq(48)
     end
   end
 end
