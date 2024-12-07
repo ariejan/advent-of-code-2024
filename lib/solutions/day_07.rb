ebbe992dead3deb36244d1d2f36c506ae298f76c
Ariejan de Vroom <ariejan@devroom.io>
2024-12-07 06:43:45 +0100

    Day 7 - Puzzle 2
    Ruby FTW

--- lib/solutions/day_07.rb
+++ lib/solutions/day_07.rb
@@ -2,19 +2,25 @@ class Day07
   attr_accessor :equations
 
   def part_one(input)
-    @equations = input.split("\n").map { |l| Equation.new(l) }
+    @equations = input.split("\n").map { |l| Equation.new(l, %i[+ *]) }
     @equations.select { |e| e.valid? }.map(&:result).sum
   end
 
   def part_two(input)
-    0
+    @equations = input.split("\n").map { |l| Equation.new(l, %i[+ * pipes]) }
+    @equations.select { |e| e.valid? }.map(&:result).sum
+  end
+end
+
+class Numeric
+  # Monkey patching to add a pipe method to numbers
+  def pipes(other)
+    (to_s + other.to_s).to_i
   end
 end
 
 class Equation
   attr_accessor :result, :inputs
-
-  OPERATORS = %i[+ *]
 
   def valid?
     try_combinations(@inputs[0], 1)
@@ -23,14 +29,15 @@ class Equation
   def try_combinations(total, idx)
     return total == @result if idx == @inputs.size
 
-    OPERATORS.each do |op|
+    @operators.each do |op|
       new_value = total.send(op, @inputs[idx])
       return true if try_combinations(new_value, idx + 1)
     end
     false
   end
 
-  def initialize(input)
+  def initialize(input, operators)
+    @operators = operators
     parts = input.split(':')
     @result = parts[0].to_i
     @inputs = parts[1].split(' ').map(&:to_i)
