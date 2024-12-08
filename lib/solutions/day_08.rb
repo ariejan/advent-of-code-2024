7cd65ad3fcbb6cd75d937664052106ee65aabfe4
Ariejan de Vroom <ariejan@devroom.io>
2024-12-08 10:08:10 +0100

    Day 8 - Puzzle 2

--- lib/solutions/day_08.rb
+++ lib/solutions/day_08.rb
@@ -13,8 +13,14 @@ class Day08
     @antinodes.flatten.count(true)
   end
 
-  def part_two(_input)
-    0
+  def part_two(input)
+    parse_input(input)
+
+    @nodes.each_pair do |_node, coords|
+      find_antinodes2(coords)
+    end
+
+    @antinodes.flatten.count(true)
   end
 
   def mark_antinode(x, y)
@@ -25,7 +31,7 @@ class Day08
 
     if x < 0 || x >= @w || y < 0 || y >= @h
       # puts '- OUT'
-      return
+      return false
     end
 
     # puts ' - OK'
@@ -40,6 +46,29 @@ class Day08
       dy = b[1] - a[1]
 
       mark_antinode(a[0] - dx, a[1] - dy)
+    end
+  end
+
+  def find_antinodes2(coords)
+    # Loop through all coordinate pemutations
+    coords.permutation(2).to_a.each do |a, b|
+      # Mark this node as antinode
+      mark_antinode(a[0], a[1])
+
+      dx = b[0] - a[0]
+      dy = b[1] - a[1]
+
+      ox = a[0]
+      oy = a[1]
+
+      multiplier = 1
+
+      loop do
+        in_grid = mark_antinode(ox - (dx * multiplier), oy - (dy * multiplier))
+        break unless in_grid
+
+        multiplier += 1
+      end
     end
   end
 
