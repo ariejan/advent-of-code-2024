11d1cc6c997426b44451ee07843bdfb5f4b345e9
Ariejan de Vroom <ariejan@devroom.io>
2024-12-10 07:06:55 +0100

    Day 9 - Puzzle 2

--- lib/solutions/day_09.rb
+++ lib/solutions/day_09.rb
@@ -2,21 +2,25 @@ class Day09
   attr_accessor :input, :disk, :checksum
 
   def part_one(input)
+    return 0
     # Parse input
     parse_input(input)
 
     # Defrag
     defrag!
-    @disk.compact!
 
     checksum!
-
-    # Calc checksum
     @checksum
   end
 
   def part_two(input)
-    0
+    parse_input(input)
+
+    # Defrag
+    defrag2!
+
+    checksum!
+    @checksum
   end
 
   def parse_input(input)
@@ -48,8 +52,43 @@ class Day09
     end
   end
 
+  def defrag2!
+    # @files is now an array of tuples: [id, start, length]
+    chunkers = @disk.map { |e| e || '.' }.chunk { |y| y }
+    _, @files = chunkers.each_with_object([0, []]) do |(k, chunk), memo|
+      memo[1] << [k, memo[0], chunk.size]
+      memo[0] += chunk.length
+    end
+
+    @inputs = @files.dup.select { |b| b[0] != '.' }.reverse
+    @inputs.each do |filler|
+      gap_needed = filler[2]
+
+      block = @files.find { |b| b[0] == '.' && b[2] >= gap_needed }
+
+      next if block.nil? || block[1] >= filler[1]
+
+      base = block[1]
+
+      # Copy data on @disk
+      0.upto(filler[2] - 1) do |i|
+        @disk[base + i] = @disk[filler[1] + i]
+        @disk[filler[1] + i] = nil
+      end
+
+      # Update @files
+      block_idx = @files.index(block)
+      @files.delete_at(block_idx)
+      @files.insert(block_idx, ['.', block[1] + filler[2], block[2] - filler[2]])
+      @files.delete(filler)
+    end
+  end
+
+  def place_file!(idx, space)
+  end
+
   def checksum!
     @checksum = 0
-    @disk.each_with_index { |id, idx| @checksum += idx * id }
+    @disk.each_with_index { |id, idx| @checksum += idx * id unless id.nil? }
   end
 end
