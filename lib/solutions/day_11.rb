bd88671beca92e9c88a91ee2aa8f7b6445f9e025
Ariejan de Vroom <ariejan@devroom.io>
2024-12-11 11:03:44 +0100

    Refactor Day 11 and Puzzle 2

--- lib/solutions/day_11.rb
+++ lib/solutions/day_11.rb
@@ -1,37 +1,66 @@
 class Day11
-  attr_accessor :stones, :blinks
+  attr_accessor :stones, :iterations, :cache
 
-  def part_one(input)
-    @stones = input.split(' ').map(&:to_i)
+  # Notes:
+  #  - Keeping input as String is faster when splitting, only convert to int when needed
+  #  - Caching is key
+  #  - Count, don't keep resulting stone values in memory.
 
-    25.times { blink! }
-
-    @stones.size
+  def initialize
+    @cache = {}
   end
 
-  def blink!
-    new_stones = []
-    @stones.each_with_index do |stone, i|
-      value_s = stone.to_s
-
-      if stone == 0
-        new_stones[i] = 1
-      elsif value_s.length.even?
-        split = value_s.length / 2
-        new_stones[i] = [value_s[0...split].to_i, value_s[split..-1].to_i]
-      else
-        new_stones[i] = stone * 2024
-      end
-    end
-    
-    @stones = new_stones.flatten!
+  def part_one(input)
+    run(input, 25)
   end
 
   def part_two(input)
-    @stones = input.split(' ').map(&:to_i)
+    run(input, 75)
+  end
 
-    75.times { blink! }
+  def run(input, iterations)
+    @stones = input.split(' ')
+    @iterations = iterations
 
-    @stones.size
+    result = 0
+    @stones.each { |s| result += count_stones_after(s, iterations) }
+    result
+  end
+
+  def count_stones_after(stone, iterations)
+    return 1 if iterations == 0
+
+    cache_key = [stone, iterations].freeze
+    return @cache[cache_key] if @cache.key?(cache_key)
+
+    next_iter = blink(stone)
+
+    total = 0
+    next_iter.each { |s| total += count_stones_after(s, iterations - 1) }
+    @cache[cache_key] = total
+    total
+  end
+
+  def blink(stone)
+    if stone == '0'
+      ['1']
+    elsif stone.size.even?
+      split_digits(stone)
+    else
+      [multiply(stone, 2024)]
+    end
+  end
+
+  def multiply(stone, factor)
+    (stone.to_i * factor).to_s
+  end
+
+  def split_digits(stone)
+    split = stone.size / 2
+    left = stone[0...split].sub(/^0+/, '') # Remove leading zeros on left half
+    left = '0' if left.empty?
+    right = stone[split..-1].sub(/^0+/, '') # Remove leading zeros on right half
+    right = '0' if right.empty?
+    [left, right]
   end
 end
--- spec/solutions/day_11_spec.rb
+++ spec/solutions/day_11_spec.rb
@@ -9,10 +9,4 @@ RSpec.describe Day11 do
       expect(subject.part_one(input)).to eq(55_312)
     end
   end
-
-  describe '#part_two' do
-    it 'calculates the correct solutions for part two' do
-      expect(subject.part_two(input)).to eq(0)
-    end
-  end
 end
