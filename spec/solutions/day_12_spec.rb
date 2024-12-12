ab2df4c083577d070dd05970df6e1a9dd32aeb91
Ariejan de Vroom <ariejan@devroom.io>
2024-12-12 06:38:59 +0100

    Day 12 - Puzzle 1

---
+++ spec/solutions/day_12_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+require 'solutions/day_12'
+
+RSpec.describe Day12 do
+  let(:input) { File.read(File.join(__dir__, '..', '..', 'spec', 'input', 'day_12_test.txt')).strip }
+
+  describe '#part_one' do
+    it 'calculates the correct solutions for part one' do
+      expect(subject.part_one(input)).to eq(1930)
+    end
+  end
+
+  describe '#part_two' do
+    it 'calculates the correct solutions for part two' do
+      expect(subject.part_two(input)).to eq(1206)
+    end
+  end
+end
